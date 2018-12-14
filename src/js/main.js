1a42c1d901ba36b1e14d6f45e57141a169e9429b
Krakovsky <work.krakovsky@gmail.com>
2018-12-14 10:54:43 +0200

    zazaa

--- src/js/main.js
+++ src/js/main.js
@@ -6,4 +6,31 @@ $(document).ready(function() {
 		.addClass('active').siblings().removeClass('active')
 		.closest('div.tabs').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
 	});
+
+	$("#phone").mask("+38(099) 999-99-99");
+
+	$(".btn-clear-filter").click(function(){
+		$(".tabs form")[0].reset();
+		$(".tabs form")[1].reset();
+	});
+
+	$(".call-me-btn").click(function(){
+		$(".modal").animate({
+			top: "-1000px"
+		}, 300);
+
+		$(".modal-call").animate({
+			top: "10%"
+		}, 300);
+
+		$(".overlay").toggle();
+	});
+
+	$(".close-modal").click(function(){
+		$(".modal").animate({
+			top: "-1000px"
+		}, 300);
+		$(".overlay").toggle();
+	});
+
 });
